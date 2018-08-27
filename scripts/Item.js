6ea618c23260f71667802cf632149e94a8a0ea7e
Chelsea Kent <chelsea.kent@gmail.com>
2018-08-27 08:43:15 -0700

    Item factory and validator

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,5 +1,23 @@
 'use strict';
 
-const Item = (function(){
-  return {};
-}());
+//Item IIFE - Item module that contains functions to validate and create items
+const Item = (function() {
+  //validate name function
+  const validateName = function(name) {
+    if (!name) throw new TypeError('Name does not exist');
+  };
+  // create function
+  const create = function(name) {
+    let item = {
+      id: cuid(),
+      name,
+      checked: false
+    };
+    return item;
+  };
+  //return to make available global scope
+  return {
+    validateName,
+    create
+  };
+})();
