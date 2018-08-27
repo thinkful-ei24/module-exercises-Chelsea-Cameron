c15d16a8970d1b7fd03a9bcaa6204b99134e5c85
Cameron Hatch <cameronlavigne92@gmail.com>
2018-08-27 10:53:00 -0500

    added item create and validate to shopping-list.js

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -1,4 +1,4 @@
-/* global store, cuid */
+/* global store, Item */
 'use strict';
 
 // eslint-disable-next-line no-unused-vars
@@ -55,7 +55,13 @@ const shoppingList = (function() {
   }
 
   function addItemToShoppingList(itemName) {
-    store.items.push({ id: cuid(), name: itemName, checked: false });
+    try {
+      Item.validateName(itemName);
+      store.items.push(Item.create(itemName));
+      render();
+    } catch (error) {
+      console.log(`Cannot add item: ${error.message}`);
+    }
   }
 
   function handleNewItemSubmit() {
